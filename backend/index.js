d9eb9ce68a7bb99f6b2ac069fbb6f3452a994fae
Risha Rout <risha.rout1607@gmail.com>
2024-06-20 13:38:51 +0530

    added login

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,13 @@ const User=require('./models/Users.js');
 const jwt = require('jsonwebtoken');
 const bcrypt= require('bcryptjs');
 const dotenv=require('dotenv');
+const cookieParser = require("cookie-parser");
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 DBConnection();
 
@@ -40,7 +44,7 @@ app.post("/register",async (req,res)=>{
         });
 
         const  token = jwt.sign({ id: user._id ,email}, process.env.SECRET_KEY,{
-            expiresIn:"1h"
+            expiresIn:"24d"
         });
         user.token=token;
         user.password=undefined;
@@ -55,6 +59,49 @@ app.post("/register",async (req,res)=>{
         console.error(error)
 
     }
+
+});
+
+app.post("/login",async (req,res)=>{
+    console.log(req);
+    try{
+   const{email ,password}=req.body;
+   if (!(email && password)){
+    return res.status(400).send("Please fill all the required fields");
+
+   }
+   const user= await User.findOne({email});
+   if (!user){
+    return res.status(401).send("User not found!");
+   }
+   const enteredPassword=await bcrypt.compare(password,user.password);
+   if (!enteredPassword){
+    return res.status(401).send("Password is incorrect!");
+   }
+   const token = await jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+    expiresIn: "24d",
+});
+user.token = token;
+user.password = undefined;
+
+const options = {
+    expires: new Date(Date.now() + 24 * 24 * 60 * 60 * 1000),
+    httpOnly: true, 
+};
+res.status(200).cookie("token", token,options ).json({
+    message: "You have successfully logged in !",
+    success:true,
+    token,
+});
+
+
+
+}
+catch (error) {
+    console.log(error.message)
+
+}
+   
 
 });
 
