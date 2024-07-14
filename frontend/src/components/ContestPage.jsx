7facc705ee4d31a856310d1508de2403c72aa61f
Risha Rout <risha.rout1607@gmail.com>
2024-07-14 16:19:11 +0530

    contest page

--- frontend/src/components/ContestPage.jsx
+++ frontend/src/components/ContestPage.jsx
@@ -26,6 +26,15 @@ const ContestPage = () => {
     fetchContests();
   }, []);
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   return (
     <div className="contest-page">
       <header>
